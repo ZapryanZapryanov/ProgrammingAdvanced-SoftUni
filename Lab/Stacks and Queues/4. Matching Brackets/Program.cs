ae3d0dbf484e7cf011f33bc9ed5f4b38e7dbd596
zaprian1223 <zaprian1223@gmail.com>
2022-05-27 16:03:54 +0300

    Added a solution

---
+++ Lab/Stacks and Queues/4. Matching Brackets/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine().Split();
+            
+            Stack<string> stack = new Stack<string>(elements.Reverse());
+
+            while (stack.Count > 1)
+            {
+                int leftNum = int.Parse(stack.Pop());
+                string symbol = stack.Pop();
+                int rightNum = int.Parse(stack.Pop());
+
+                if (symbol == "+")
+                {
+                    stack.Push((leftNum + rightNum).ToString());
+                }
+                else if (symbol == "-")
+                {
+                    stack.Push((leftNum - rightNum).ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}

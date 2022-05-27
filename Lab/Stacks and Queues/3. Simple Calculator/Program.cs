e1e36d519b0b7b7d14862d3f9ccecfbf647446db
zaprian1223 <zaprian1223@gmail.com>
2022-05-27 16:03:35 +0300

    Added a solution

---
+++ Lab/Stacks and Queues/3. Simple Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cmd = Console.ReadLine().Split();
+
+            List<int> list = new List<int>();
+            List<string> symbol = new List<string>();
+            int firstElements = 0;
+            int secondElements = 0;
+            
+            foreach (string item in cmd)
+            {
+                if (item != "-" && item != "+")
+                {
+                    list.Add(int.Parse(item));
+                }
+                else
+                {
+                    symbol.Add(item);
+                }
+              
+               
+            }
+            foreach (var item in symbol)
+            {
+                if (item == "-")
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+          
+            Console.WriteLine(sum);
+        }
+    }
+}

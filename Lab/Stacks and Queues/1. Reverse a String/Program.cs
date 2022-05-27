c5a88d5fd0faed24561b23e1df0a825ebf17ed95
zaprian1223 <zaprian1223@gmail.com>
2022-05-27 16:02:31 +0300

    Added a solution

---
+++ Lab/Stacks and Queues/1. Reverse a String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _1._Reverse_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char item in input)
+            {
+                stack.Push(item);
+            }
+            Console.WriteLine(string.Join("", stack));
+        }
+    }
+}

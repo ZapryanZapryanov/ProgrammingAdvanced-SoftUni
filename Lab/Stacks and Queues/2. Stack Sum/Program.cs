deb2d53998ac24c8ce054a66ab877e52eec27260
zaprian1223 <zaprian1223@gmail.com>
2022-05-27 16:03:01 +0300

    Added a solution

---
+++ Lab/Stacks and Queues/2. Stack Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(num);
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] elements = input.Split();
+                string cmdArg = elements[0].ToLower();
+                if (cmdArg == "add")
+                {
+                    int firstNum = int.Parse(elements[1]);
+                    int secondNum = int.Parse(elements[2]);
+                    stack.Push(firstNum);
+                    stack.Push(secondNum);
+                }
+                else if (cmdArg == "remove")
+                {
+                    int count = int.Parse(elements[1]);
+                    if (count <= stack.Count)
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            int sum = stack.Sum();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}

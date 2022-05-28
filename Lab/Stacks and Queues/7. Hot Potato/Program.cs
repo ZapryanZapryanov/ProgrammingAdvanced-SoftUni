a9b35c451c29336cb6bed34bd19f57850aa7f319
zaprian1223 <zaprian1223@gmail.com>
2022-05-28 14:46:10 +0300

    Added a solution

---
+++ Lab/Stacks and Queues/7. Hot Potato/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] children = Console.ReadLine().Split();
+            int count = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(children);
+
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                     
+                    string name = queue.Dequeue();
+                    if (i == count)
+                    {
+                        Console.WriteLine($"Removed {name}");
+                        break;
+                    }
+                    queue.Enqueue(name);
+                }
+            }
+           
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}

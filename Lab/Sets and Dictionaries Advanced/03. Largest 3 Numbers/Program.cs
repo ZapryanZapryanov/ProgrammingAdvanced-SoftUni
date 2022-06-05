5938c238bccfe759fa47ffb4e8afb452c88485a1
zaprian1223 <zaprian1223@gmail.com>
2022-06-05 18:40:10 +0300

    Added a solution

---
+++ Lab/Sets and Dictionaries Advanced/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var num = Console.ReadLine().Split(' ').Select(int.Parse)
+            .OrderByDescending(x => x).ToList().Take(3);
+            Console.WriteLine(string.Join(" ", num));
+
+        }
+    }
+}

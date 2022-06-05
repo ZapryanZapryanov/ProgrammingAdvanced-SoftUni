9d7de61e674ed2517d1c3699c1b0302fa43d3736
zaprian1223 <zaprian1223@gmail.com>
2022-06-05 18:39:53 +0300

    Added a solution

---
+++ Lab/Sets and Dictionaries Advanced/02. Average Student Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] student = Console.ReadLine().Split();
+                string nameOfStudent = student[0];
+                decimal grade = decimal.Parse(student[1]);
+                if (students.ContainsKey(nameOfStudent))
+                {
+                    students[nameOfStudent].Add(grade);
+                }
+                else
+                {
+                    students.Add(nameOfStudent, new List<decimal> { grade });
+                }
+            }
+            foreach (var item in students)
+            {
+               
+                Console.WriteLine($"{item.Key} -> {string.Join(" ", item.Value.Select(x => x.ToString("f2")))} (avg: {item.Value.Average():f2})");
+            }
+        }
+    }
+}

528d059b6226dbd5a5ded0830f5fb9b3a4396961
zaprian1223 <zaprian1223@gmail.com>
2022-06-06 13:24:14 +0300

    Added a solution

---
+++ Lab/Sets and Dictionaries Advanced/07. Parking Lot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            HashSet<string> cars = new HashSet<string>();
+            while (input != "END")
+            {
+                string[] cmdArg = input.Split(", ");
+                string firstElement = cmdArg[0];
+                string secondElement = cmdArg[1];
+                if (firstElement == "IN")
+                {
+                    cars.Add(secondElement);
+                }
+                else if (firstElement == "OUT")
+                {
+                    cars.Remove(secondElement);
+                }
+
+                input = Console.ReadLine();
+            }
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, cars));
+            }
+            
+        }
+    }
+}

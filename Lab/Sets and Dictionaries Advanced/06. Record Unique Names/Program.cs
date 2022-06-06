6b3127ef3a528fc0e747e83612c42416ae29258c
zaprian1223 <zaprian1223@gmail.com>
2022-06-06 13:24:00 +0300

    Added a solution

---
+++ Lab/Sets and Dictionaries Advanced/06. Record Unique Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                names.Add(input);
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+            
+        }
+    }
+}

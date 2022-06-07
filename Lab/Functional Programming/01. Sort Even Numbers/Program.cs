e8b20358f7b43bb9e5ef02e79e2717ebb6dd271d
zaprian1223 <zaprian1223@gmail.com>
2022-06-07 16:05:34 +0300

    Added a solution

---
+++ Lab/Functional Programming/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x).ToArray();
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}

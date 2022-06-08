35c39b5acb6e858ac92c654eb6adf32925b2c631
zaprian1223 <zaprian1223@gmail.com>
2022-06-08 13:39:27 +0300

    Added a solution

---
+++ Lab/Functional Programming/04. Add VAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(", ").Select(double.Parse).Select(sumWithVat).ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]:f2}");
+            }
+           
+        }
+        static Func<double, double> sumWithVat = x => x * 1.20;
+    }
+}

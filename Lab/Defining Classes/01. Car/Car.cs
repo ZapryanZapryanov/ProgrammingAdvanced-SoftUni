013f3ff4b6630e7d2a23bfc2f6c81e20fc6f3bcc
zaprian1223 <zaprian1223@gmail.com>
2022-06-09 11:24:48 +0300

    Added a solution

---
+++ Lab/Defining Classes/01. Car/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int years;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Years
+        {
+            get { return this.years; }
+            set { this.years = value; }
+        }
+
+
+
+
+    }
+}
---
+++ Lab/Defining Classes/01. Car/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+  public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car cars = new Car();
+            cars.Make = "VW";
+            cars.Model = "MK3";
+            cars.Years = 1992;
+            Console.WriteLine($"Make: {cars.Make}\nModel: {cars.Model}\nYears: {cars.Years}");
+
+            Car carsOne = new Car();
+            cars.Make = "Audi";
+            cars.Model = "A3";
+            cars.Years = 2000;
+            Console.WriteLine($"Make: {cars.Make}\nModel: {cars.Model}\nYears: {cars.Years}");
+
+        }
+    }
+}

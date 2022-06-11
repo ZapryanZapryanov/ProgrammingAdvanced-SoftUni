1d9f3dac7231a59f438edf5cba71eff5d9da9ced
zaprian1223 <zaprian1223@gmail.com>
2022-06-11 15:01:09 +0300

    Added a solution

---
+++ Lab/Generics/01. Box/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public class Box<T>
+    {
+        private List<T> internalList = new List<T>();
+
+        public void Add(T element)
+        {
+            internalList.Add(element);
+        }
+
+        public int Count => internalList.Count;
+
+        public T Remove()
+        {
+            T element = internalList[internalList.Count - 1];
+            internalList.RemoveAt(internalList.Count - 1);
+            return element;
+        }
+    }
+}

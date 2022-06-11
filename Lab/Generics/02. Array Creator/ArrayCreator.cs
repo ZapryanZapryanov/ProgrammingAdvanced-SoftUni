69a0a8540a58546099d5d65cd2a0b9a97c486e28
zaprian1223 <zaprian1223@gmail.com>
2022-06-11 15:01:24 +0300

    Added a solution

---
+++ Lab/Generics/02. Array Creator/ArrayCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace GenericsDemo
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght)
+            where T : Stream, new()
+        {
+            T[] array = new T[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = new T();
+            }
+
+            return array;
+        }
+    }
+}

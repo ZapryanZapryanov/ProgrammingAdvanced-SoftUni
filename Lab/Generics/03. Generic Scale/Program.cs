c4c38dfdc35bbb79cc99a7a5d553be2ce1bbe1da
zaprian1223 <zaprian1223@gmail.com>
2022-06-11 15:01:42 +0300

    Added a solution

---
+++ Lab/Generics/03. Generic Scale/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace GenericsDemo
+{
+    class Pair<T>
+    {
+        public Pair()
+        {
+
+        }
+
+        public T First { get; set; }
+
+        public T Second { get; set; }
+    }
+
+    class TypeWithConstructor
+    {
+        public TypeWithConstructor(int a)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Intelli code (ML)
+
+            var pair = new Pair<string>();
+
+            var list = new List<string>();
+
+            var arrayStr = CreateArray(10, "Niki");
+            var arrayStr2 = CreateArray(10, "Niki");
+        }
+
+        static T[] CreateArray<T>(int count, T element)
+        {
+            T[] result = new T[count];
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = element;
+            }
+
+            return result;
+        }
+    }
+}

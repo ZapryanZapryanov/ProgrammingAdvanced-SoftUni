45eb5060fccf09448463a907932b838824056ac7
zaprian1223 <zaprian1223@gmail.com>
2022-06-13 13:00:33 +0300

    Added a solution

---
+++ Lab/Streams, Files and Directories/02. Line Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _02._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = Path.Combine("data", "input.txt");
+            using StreamReader sr = new StreamReader(inputFile);
+            var outputFile = Path.Combine("data", "output.txt");
+            using StreamWriter sw = new StreamWriter(outputFile);
+            int lineCounter = 1;
+
+            string input;
+            while ((input = sr.ReadLine()) != null)
+            {
+                sw.WriteLine($"{lineCounter}. {input}");
+                lineCounter++;
+            }
+        }
+    }
+}

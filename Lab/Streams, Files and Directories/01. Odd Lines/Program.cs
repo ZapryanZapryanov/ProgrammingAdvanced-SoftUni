0c547c35559c673debeefd0ed831b59a101fda37
zaprian1223 <zaprian1223@gmail.com>
2022-06-13 13:00:18 +0300

    Added a solution

---
+++ Lab/Streams, Files and Directories/01. Odd Lines/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _01._Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = Path.Combine("data", "input.txt");
+            using StreamReader sr = new StreamReader(inputFile);
+            var outputFile = Path.Combine("data", "output.txt");
+            using StreamWriter sw = new StreamWriter(outputFile);
+            int lineCounter = 0;
+
+            string input;
+            while ((input = sr.ReadLine()) != null)
+            {
+                if (lineCounter % 2 != 0)
+                {
+                    sw.WriteLine(input);
+                }
+
+                lineCounter++;
+            }
+        }
+    }
+}

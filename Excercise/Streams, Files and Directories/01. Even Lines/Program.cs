503209850f02b81f69cba36ca9f8d2b62529494f
zaprian1223 <zaprian1223@gmail.com>
2022-06-17 18:39:22 +0300

    Added a solution

---
+++ Excercise/Streams, Files and Directories/01. Even Lines/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01._Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"C:\Users\ZAPRYAN\Desktop\text.txt";
+            using (StreamReader reader = new StreamReader(pattern))
+            {
+               string line = reader.ReadLine();
+                int evenOrOdd = -1;
+                while (line != null)
+                {
+                    evenOrOdd++;
+                    if (evenOrOdd % 2 == 0)
+                    {
+                        line = GetReplaceStr(line);
+                        line = GetReverseStr(line);
+                        Console.WriteLine(line);
+
+                    }
+                    line = reader.ReadLine();
+                    
+                   
+                }
+            }
+        }
+        static string GetReplaceStr(string line)
+        {
+            return line.Replace("-", "@")
+                .Replace(",", "@")
+                .Replace(".", "@")
+                .Replace(".", "@")
+                .Replace("!", "@")
+                .Replace("?", "@");
+        }
+        static string GetReverseStr(string line)
+        {
+            return string.Join(" ", line.Split(" ").Reverse());
+        }
+    }
+
+}

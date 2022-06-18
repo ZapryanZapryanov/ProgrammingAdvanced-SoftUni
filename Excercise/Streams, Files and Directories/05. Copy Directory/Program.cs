a675261b52463972b9db98b16ea689f9e2410b92
zaprian1223 <zaprian1223@gmail.com>
2022-06-18 15:50:04 +0300

    Added a solution

---
+++ Excercise/Streams, Files and Directories/05. Copy Directory/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace _05._Copy_Directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+            
+            Directory.CreateDirectory(outputPath);
+           
+            string[] files = Directory.GetFiles(inputPath);
+
+          
+            foreach (string file in files)
+            {
+                var fileName = Path.GetFileName(file); 
+                var copyDestination = Path.Combine(outputPath, fileName);
+                File.Copy(fileName, copyDestination);
+            }
+        }
+    }
+}

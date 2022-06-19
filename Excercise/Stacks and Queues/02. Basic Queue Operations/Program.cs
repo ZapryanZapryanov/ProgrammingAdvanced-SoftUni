b62304ee20d5c85dd4d9c2dba09427ba7fa2517f
zaprian1223 <zaprian1223@gmail.com>
2022-06-19 14:48:57 +0300

    Added a solution

---
+++ Excercise/Stacks and Queues/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine().Split();
+            List<int> listByNums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int n = int.Parse(elements[0]);
+            int s = int.Parse(elements[1]);
+            int x = int.Parse(elements[2]);
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(listByNums[i]);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (queue.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+
+        }
+    }
+}

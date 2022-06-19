9d426bae12a0a84add26fd17e210d8ea5c583873
zaprian1223 <zaprian1223@gmail.com>
2022-06-19 14:48:42 +0300

    Added a solution

---
+++ Excercise/Stacks and Queues/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine().Split();
+            List<int> listByNums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int n = int.Parse(elements[0]);
+            int s = int.Parse(elements[1]);
+            int x = int.Parse(elements[2]);
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(listByNums[i]);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (stack.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+           
+        }
+    }
+}

7d16d34ac2bbb57f228d9a6ff2c62613abca3a0c
zaprian1223 <zaprian1223@gmail.com>
2022-06-21 13:24:06 +0300

    Added a solution

---
+++ Excercise/Stacks and Queues/06. Songs Queue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ");
+            Queue<string> queueSongs = new Queue<string>(songs);
+
+            while (queueSongs.Count > 0)
+            {
+                string[] cmdArg = Console.ReadLine().Split(" ");
+                if (cmdArg[0] == "Play")
+                {
+                    queueSongs.Dequeue();
+                }
+                else if (cmdArg[0] == "Add")
+                {
+                    string currSong = string.Join(" ", cmdArg);
+                    string newStr = currSong.Substring(4);
+                    if (queueSongs.Contains(newStr))
+                    {
+                        Console.WriteLine($"{newStr} is already contained!");
+                    }
+                    else
+                    {
+                        queueSongs.Enqueue(newStr);
+                    }
+                }
+                else if (cmdArg[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", queueSongs));
+                }
+                
+            }
+            Console.WriteLine("No more songs!");
+        }
+      
+    }
+}

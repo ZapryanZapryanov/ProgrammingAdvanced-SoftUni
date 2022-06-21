fc983f645ea4b4674d5090875484528cba48a749
zaprian1223 <zaprian1223@gmail.com>
2022-06-21 13:23:49 +0300

    Added a solution

---
+++ Excercise/Stacks and Queues/05. Fashion Boutique/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] quantityOfClothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int capacityOfRack = int.Parse(Console.ReadLine());
+
+            Stack<int> clothesStack = new Stack<int>(quantityOfClothes);
+
+            int countRack = 1;
+            int sumClothes = 0;
+
+
+            while (clothesStack.Count > 0)
+            {
+                var currElement = clothesStack.Pop(); 
+                if (sumClothes + currElement <= capacityOfRack)     
+                {
+                    sumClothes += currElement;                  
+                }
+                else
+                {
+                    sumClothes = currElement;       
+                    countRack++;
+                }
+            }
+
+           
+            Console.WriteLine(countRack);
+        }
+    }
+}

1d44fb0dfc43d1852adbc4f30b8fb4830faecbd3
zaprian1223 <zaprian1223@gmail.com>
2022-06-22 17:45:47 +0300

    Added a solution

---
+++ Excercise/Stacks and Queues/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+          
+
+            Stack<char> stack = new Stack<char>(input);
+
+            int flag = 0;
+            foreach (var item in input)
+            {
+                
+                
+                char firstElement = stack.Pop();
+                if ((item == '[' || item == ']') && (firstElement == '[' || firstElement == ']'))
+                {
+                    
+                    continue;
+                }
+                else if ((item == '(' || item == ')') && (firstElement == '(' || firstElement == ')'))
+                {
+                  
+                    continue;
+                }
+                else if ((item == '{' || item == '}') && (firstElement == '{' || firstElement == '}'))
+                {
+                   
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+                
+                
+            }
+           
+                Console.WriteLine("YES");
+            
+            
+        }
+    }
+}

82e76901ecf6b318ad4299aa57960f615b506ac4
zaprian1223 <zaprian1223@gmail.com>
2022-06-24 16:12:35 +0300

    Added a solution

---
+++ Excercise/Functional Programming/01. Action Print/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] element = Console.ReadLine().Split().ToArray();
+            Action<string> names = x => Console.WriteLine(x);
+            foreach (var item in element)
+            {
+                names(item);
+            }
+        }
+    }
+}

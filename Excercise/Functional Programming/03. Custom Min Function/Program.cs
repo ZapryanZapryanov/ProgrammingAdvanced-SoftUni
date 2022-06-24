39f43969dfd867464bdf5feb958bb4aed62dfc60
zaprian1223 <zaprian1223@gmail.com>
2022-06-24 16:13:08 +0300

    Added a solution

---
+++ Excercise/Functional Programming/03. Custom Min Function/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrByNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], int> min = x => x.Min();
+            int numb = min(arrByNumbers);
+            Console.WriteLine(numb);
+        }
+    }
+}

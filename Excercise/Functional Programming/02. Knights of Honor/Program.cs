ad7a4aae7956eb1e8397cab8bc917ec4c4862bea
zaprian1223 <zaprian1223@gmail.com>
2022-06-24 16:12:51 +0300

    Added a solution

---
+++ Excercise/Functional Programming/02. Knights of Honor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine().Split();
+
+            Action<string> collection = names => Console.WriteLine($"Sir {names}");
+            foreach (var item in elements)
+            {
+                collection(item);
+            }
+        }
+    }
+}

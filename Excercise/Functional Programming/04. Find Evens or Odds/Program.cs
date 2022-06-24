f6c4678825a7935925479eb295ec34c86b68f617
zaprian1223 <zaprian1223@gmail.com>
2022-06-24 16:13:28 +0300

    Added a solution

---
+++ Excercise/Functional Programming/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] element = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int startIndex = element[0];
+            int endIndex = element[1];
+
+            List<int> nums = new List<int>();
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                nums.Add(i);
+            }
+            string comand = Console.ReadLine();
+      
+            if (comand == "even")
+            {
+                Console.WriteLine(string.Join(" ", nums.FindAll(x => x % 2 == 0)));
+            }
+            else if (comand == "odd")
+            {
+                Console.WriteLine(string.Join(" ", nums.FindAll(x => x % 2 != 0)));
+
+
+            }
+        }
+    }
+}

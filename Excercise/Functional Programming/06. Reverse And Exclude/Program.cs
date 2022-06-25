a27ce4d1cb979ea523919656387857e974aa899a
zaprian1223 <zaprian1223@gmail.com>
2022-06-25 13:03:46 +0300

    Added a solution

---
+++ Excercise/Functional Programming/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int[]> reverse = i =>
+            {
+                for (int j = 0; j < i.Length / 2; j++)
+                {
+                    int temp = i[j];
+                    i[j] = i[i.Length - 1 - j];
+                    i[i.Length - 1 - j] = temp;
+                }
+            };
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+            Predicate<int> filter = i => i % n != 0;
+            reverse(numbers);
+            Console.WriteLine(string.Join(" ", numbers.ToList().FindAll(filter)));
+        }
+    }
+}

cc4e4fa42c3e3c053b2b7fd48607386d6a953469
zaprian1223 <zaprian1223@gmail.com>
2022-06-25 13:04:08 +0300

    Added a solution

---
+++ Excercise/Functional Programming/07. Predicate For Names/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Predicate<string> cond = str => str.Length <= n;
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList().FindAll(cond).ForEach(Console.WriteLine);
+
+
+
+        }
+    }
+}

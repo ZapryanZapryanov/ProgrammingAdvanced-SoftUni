bc80c5d92a9ce1c2f8c3af7e9fe9f4d682c7b5ba
zaprian1223 <zaprian1223@gmail.com>
2022-06-26 18:29:49 +0300

    Added a solution

---
+++ Excercise/Functional Programming/11. TriFunction/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _11._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int treshold = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ");
+            Console.WriteLine(names.First(name => name.Select(symbol => (int)symbol).Sum() >= treshold));
+        }
+    }
+}

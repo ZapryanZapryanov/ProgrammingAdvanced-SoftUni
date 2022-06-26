e49b0ba57bb9c196b7f28c580d43e9701750002d
zaprian1223 <zaprian1223@gmail.com>
2022-06-26 18:29:00 +0300

    Added a solution

---
+++ Excercise/Functional Programming/08. List Of Predicates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> dividers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+            List<int> numbers = new List<int>(); 
+            for (int number = 1; number <= n; number++)
+            {
+                numbers.Add(number);
+            }
+
+            List<int> printNumbers = new List<int>();
+
+            foreach (int number in numbers)
+            {
+                bool isDivisible = true;
+               
+                foreach (int divider in dividers)
+                {
+                    
+                    Predicate<int> divisible = number => number % divider == 0;
+                    
+                    if (!divisible(number)) 
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    printNumbers.Add(number);
+
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", printNumbers));
+        }
+    }
+}

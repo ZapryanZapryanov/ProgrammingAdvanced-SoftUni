4d4f993a941db21e0290bc916891191b7e06ddd5
zaprian1223 <zaprian1223@gmail.com>
2022-06-30 17:30:35 +0300

    Added a solution

---
+++ Excercise/Generics/03. Generic Swap Method String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Generic_Swap_Method_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            var currBox = new Box<string>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                string elements = Console.ReadLine();
+
+
+                currBox.Name.Add(elements);
+            }
+            int[] swap = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int firstIndex = swap[0];
+            int secondIndex = swap[1];
+            currBox.SwapElements(firstIndex, secondIndex);
+            Console.WriteLine(currBox);
+            
+            
+
+
+        }
+    }
+}

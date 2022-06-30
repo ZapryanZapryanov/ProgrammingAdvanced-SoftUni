511a9f00db8ec4b96a882222c955e5601ccaff83
zaprian1223 <zaprian1223@gmail.com>
2022-06-30 17:29:52 +0300

    Added a solution

---
+++ Excercise/Generics/01. Generic Box of String/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Generic_Box_of_String
+{
+   public class Box<T>
+    {
+        private T name;
+
+        public T Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Box(T name)
+        {
+            this.Name = name;
+        }
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Name}";
+        }
+    }
+}
---
+++ Excercise/Generics/01. Generic Box of String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Generic_Box_of_String
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string elements = Console.ReadLine();
+                var currBox = new Box<string>(elements);
+                Console.WriteLine(currBox);
+            }
+        }
+    }
+}

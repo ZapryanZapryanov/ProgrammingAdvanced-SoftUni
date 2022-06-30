170cb1c9714d3b67999a8a99138698ff39701d89
zaprian1223 <zaprian1223@gmail.com>
2022-06-30 17:30:16 +0300

    Added a solution

---
+++ Excercise/Generics/02. Generic Box of Integer/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02._Generic_Box_of_Integer
+{
+   public class Box<T>
+    {
+        private T name;
+
+        public T Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Box(T name)
+        {
+            this.Name = name;
+        }
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Name}";
+        }
+    }
+}
---
+++ Excercise/Generics/02. Generic Box of Integer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02._Generic_Box_of_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                var currBox = new Box<int>(elements);
+                Console.WriteLine(currBox);
+            }
+        }
+    }
+}

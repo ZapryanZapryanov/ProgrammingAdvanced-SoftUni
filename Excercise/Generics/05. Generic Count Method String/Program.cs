a356c5f8291659c9713b8da650d0d415f0a12b30
zaprian1223 <zaprian1223@gmail.com>
2022-07-01 21:50:27 +0300

    Added a solution

---
+++ Excercise/Generics/05. Generic Count Method String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Generic_Count_Method_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfLine = int.Parse(Console.ReadLine());
+
+            var list = new List<string>();
+            for (int i = 0; i < numberOfLine; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            var box = new Box<string>(list);
+            var elementToCompare = Console.ReadLine();
+            var count = box.CountOFGreaterElements(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}

3cd1d080a42c6356ad9fd4eed97e8535fe15c042
zaprian1223 <zaprian1223@gmail.com>
2022-07-01 21:50:09 +0300

    Added a solution

---
+++ Excercise/Generics/04. Generic Swap Method Integer/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04._Generic_Swap_Method_Integer
+{
+   public class Box<T>
+    {
+        private List<T> name;
+
+        public List<T> Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Box()
+        {
+            this.Name = new List<T>();
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in Name)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+        public void SwapElements(int firstIndex, int secondIndex)
+        {
+            T firstElement = Name[firstIndex];
+            T secondElement = Name[secondIndex];
+            Name[secondIndex] = firstElement;
+            Name[firstIndex] = secondElement;
+            
+
+
+        }
+    }
+}
---
+++ Excercise/Generics/04. Generic Swap Method Integer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Generic_Swap_Method_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var currBox = new Box<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+
+
+                currBox.Name.Add(elements);
+            }
+            int[] swap = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int firstIndex = swap[0];
+            int secondIndex = swap[1];
+            currBox.SwapElements(firstIndex, secondIndex);
+            Console.WriteLine(currBox);
+        }
+    }
+}

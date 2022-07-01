b4c7f7f5a96c1055f8397342f3e9dbd25ccbc5aa
zaprian1223 <zaprian1223@gmail.com>
2022-07-01 21:51:41 +0300

    Added a solution

---
+++ Excercise/Generics/08. Threeuple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _08._Threeuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elemet = Console.ReadLine().Split();
+            string firstNameAndLast = $"{elemet[0]} {elemet[1]}";
+            string adress = elemet[2];
+            string town = elemet[3];
+
+            string[] newElement = Console.ReadLine().Split();
+            string name = newElement[0];
+            int drinkBeer = int.Parse(newElement[1]);
+            bool drunkOrNo = newElement[2] == "drunk" ? true : false;
+
+            string[] lastElement = Console.ReadLine().Split();
+            string item = lastElement[0];
+            double newItem = double.Parse(lastElement[1]);
+            string bankName = lastElement[2];
+
+            var item1 = new Tuple<string, string, string>(firstNameAndLast, adress, town);
+            Console.WriteLine(item1);
+
+            var item2 = new Tuple<string, int, bool>(name, drinkBeer, drunkOrNo);
+            Console.WriteLine(item2);
+
+            var item3 = new Tuple<string, double, string>(item, newItem, bankName);
+            Console.WriteLine(item3);
+        }
+    }
+}
---
+++ Excercise/Generics/08. Threeuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._Threeuple
+{
+    class Tuple<TFirst, TSecond, TThree>
+    {
+        public TFirst Type1 { get; set; }
+        public TSecond Type2 { get; set; }
+        public TThree Type3 { get; set; }
+        public Tuple(TFirst item1, TSecond item2, TThree type3)
+        {
+            this.Type1 = item1;
+            this.Type2 = item2;
+            this.Type3 = type3;
+        }
+        public override string ToString()
+        {
+            return $"{Type1} -> {Type2} -> {Type3}";
+        }
+    }
+}

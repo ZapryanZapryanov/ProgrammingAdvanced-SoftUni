3bce6f1f147f0f692ca7a5f7b5b058e9196c9be0
zaprian1223 <zaprian1223@gmail.com>
2022-07-01 21:51:08 +0300

    Added a solution

---
+++ Excercise/Generics/07. Tuple/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07._Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elemet = Console.ReadLine().Split();
+            string firstNameAndLast = $"{elemet[0]} {elemet[1]}";
+            string adress = elemet[2];
+
+            string[] newElement = Console.ReadLine().Split();
+            string name = newElement[0];
+            int drinkBeer = int.Parse(newElement[1]);
+
+            string[] lastElement = Console.ReadLine().Split();
+            int item = int.Parse(lastElement[0]);
+            double newItem = double.Parse(lastElement[1]);
+
+            var item1 = new Tuple<string, string>(firstNameAndLast, adress);
+            Console.WriteLine(item1);
+
+            var item2 = new Tuple<string, int>(name, drinkBeer);
+            Console.WriteLine(item2);
+
+            var item3 = new Tuple<int, double>(item, newItem);
+            Console.WriteLine(item3);
+
+        }
+    }
+}
---
+++ Excercise/Generics/07. Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._Tuple
+{
+    class Tuple<TFirst, TSecond>
+    {
+        public TFirst Type1 { get; set; }
+        public TSecond Type2 { get; set; }
+        public Tuple(TFirst item1, TSecond item2)
+        {
+            this.Type1 = item1;
+            this.Type2 = item2;
+        }
+        public override string ToString()
+        {
+            return $"{Type1} -> {Type2}";
+        }
+    }
+}

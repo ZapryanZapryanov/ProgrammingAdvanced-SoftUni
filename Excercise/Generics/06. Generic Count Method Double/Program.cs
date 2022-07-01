771f4f5ecf39b156e63f5f02b3fcbf441b9f59b2
zaprian1223 <zaprian1223@gmail.com>
2022-07-01 21:50:44 +0300

    Added a solution

---
+++ Excercise/Generics/06. Generic Count Method Double/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Generic_Count_Method_Double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfLine = int.Parse(Console.ReadLine());
+
+            var list = new List<double>();
+            for (int i = 0; i < numberOfLine; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+
+            var box = new Box<double>(list);
+            var elementToCompare = double.Parse(Console.ReadLine());
+            var count = box.CountOFGreaterElements(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}

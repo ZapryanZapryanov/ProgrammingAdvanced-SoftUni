b08cde6782cbb1cd7aee5abea76cc001ff1d547f
zaprian1223 <zaprian1223@gmail.com>
2022-07-03 15:31:29 +0300

    Added a solution

---
+++ Excercise/Iterators and Comparators/02. Collection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02._Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            ListyIterator<string> listy = null;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var tokens = command.Split();
+
+                if (tokens[0] == "Create")
+                {
+                    listy = new ListyIterator<string>(tokens.Skip(1).ToArray());
+                }
+                else if (tokens[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (tokens[0] == "Print")
+                {
+                    listy.Print();
+                }
+                else if (tokens[0] == "PrintAll")
+                {
+                    listy.PrintAll();
+                }
+                else if (tokens[0] == "HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+            }
+        }
+    }
+}

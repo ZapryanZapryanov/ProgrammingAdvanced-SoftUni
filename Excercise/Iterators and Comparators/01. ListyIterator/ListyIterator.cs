270a9dedae6d117cd58a9562a1cc1d7c608e57da
zaprian1223 <zaprian1223@gmail.com>
2022-07-03 15:31:11 +0300

    Added a solution

---
+++ Excercise/Iterators and Comparators/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._ListyIterator
+{
+    class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+        private int currIndex;
+
+        public ListyIterator(params T[] data)
+        {
+            collection = new List<T>(data);
+            currIndex = 0;
+        }
+
+        public bool HasNext() => currIndex < collection.Count - 1;
+
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                currIndex++;
+            }
+            return canMove;
+        }
+
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation");
+            }
+
+            Console.WriteLine($"{collection[currIndex]}");
+        }
+
+        public void PrintAll() => Console.WriteLine(string.Join(" ", collection));
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T element in collection)
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}

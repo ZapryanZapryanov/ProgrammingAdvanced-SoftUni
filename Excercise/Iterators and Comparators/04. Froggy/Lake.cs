8fafe2ff1cc5551fbd9c192c07f01dff3c93933c
zaprian1223 <zaprian1223@gmail.com>
2022-07-04 14:56:57 +0300

    Added a solution

---
+++ Excercise/Iterators and Comparators/04. Froggy/Lake.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._Froggy
+{
+   public class Lake : IEnumerable<int>
+    {
+        private int[] lake;
+
+        public Lake(params int[] rocks)
+        {
+            lake = rocks;
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < lake.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return lake[i];
+                }
+            }
+
+            for (int i = lake.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return lake[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Excercise/Iterators and Comparators/04. Froggy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _04._Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lake = new Lake(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}

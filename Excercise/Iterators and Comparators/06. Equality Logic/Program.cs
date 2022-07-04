6beccf64a0b07412876bc834961ca8d3a4c88c2a
zaprian1223 <zaprian1223@gmail.com>
2022-07-04 14:57:52 +0300

    Added a solution

---
+++ Excercise/Iterators and Comparators/06. Equality Logic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06._Equality_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sortedSet = new SortedSet<Person>();
+            var hasSet = new HashSet<Person>();
+            var lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var person = new Person(tokens[0], int.Parse(tokens[1]));
+                sortedSet.Add(person);
+                hasSet.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hasSet.Count);
+        }
+    }
+}

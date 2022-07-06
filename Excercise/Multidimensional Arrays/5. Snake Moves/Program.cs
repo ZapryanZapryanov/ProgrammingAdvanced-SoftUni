52f40509fec0b3b2ed65d097ccdfa1b218df4120
zaprian1223 <zaprian1223@gmail.com>
2022-07-06 18:11:06 +0300

    Added a solution

---
+++ Excercise/Multidimensional Arrays/5. Snake Moves/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowAndCol = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string input = Console.ReadLine();
+            int count = 0;
+            List<string> listWords = new List<string>();
+            foreach (char item in input)
+            {
+                listWords.Add(item.ToString());
+            }
+            string[,] matrix = new string[rowAndCol[0], rowAndCol[1]];
+            
+
+            for (int i = 0; i < rowAndCol[0]; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int a = 0; a < rowAndCol[1]; a++)
+                    {
+                        
+                        matrix[i, a] = listWords[count];
+                        count++;
+                        if (count == listWords.Count)
+                        {
+                            count = 0;
+                        }
+
+                    }
+                    
+                }
+                else
+                {
+                    for (int r = rowAndCol[1] - 1; r >= 0; r--)
+                    {
+                       
+                        matrix[i, r] = listWords[count];
+                        count++;
+                        if (count == listWords.Count)
+                        {
+                            count = 0;
+                        }
+
+                    }
+                }
+            }
+            for (int i = 0; i < rowAndCol[0]; i++)
+            {
+                for (int a = 0; a < rowAndCol[1]; a++)
+                {
+                    Console.Write(matrix[i,a]);
+                }
+                Console.WriteLine();
+            }
+        }
+       
+        
+    }
+}

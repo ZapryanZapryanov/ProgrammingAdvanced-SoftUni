e35c85fd25a9179d3a4cdd8482a55f528cca5b48
zaprian1223 <zaprian1223@gmail.com>
2022-07-08 12:43:42 +0300

    Added a solution

---
+++ Excercise/Sets and Dictionaries Advanced/01. Unique Usernames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string username = Console.ReadLine();
+                usernames.Add(username);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, usernames));
+        }
+    }
+}

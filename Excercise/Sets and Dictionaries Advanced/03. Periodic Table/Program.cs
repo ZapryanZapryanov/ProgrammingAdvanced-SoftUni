69fc96117c9a491592923fc31465b59f45a570d2
zaprian1223 <zaprian1223@gmail.com>
2022-07-08 12:44:27 +0300

    Added a solution

---
+++ Excercise/Sets and Dictionaries Advanced/03. Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+            int n = int.Parse(Console.ReadLine()); 
+
+            for (int row = 1; row <= n; row++)
+            {
+                string input = Console.ReadLine(); //"Mo O Ce"
+                string[] chElements = input.Split(" "); //["Mo", "O", "Ce"]
+                foreach (string chElement in chElements)
+                {
+                    elements.Add(chElement);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}

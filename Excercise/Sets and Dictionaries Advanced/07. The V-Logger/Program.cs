003bd1c287b3f298a99da56570636db1b9902c2c
zaprian1223 <zaprian1223@gmail.com>
2022-07-09 16:22:06 +0300

    Added a solution

---
+++ Excercise/Sets and Dictionaries Advanced/07. The V-Logger/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._The_V_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = Console.ReadLine().ToCharArray();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArg = Console.ReadLine().Split();
+                string position = cmdArg[0];
+
+            }
+        }
+    }
+}

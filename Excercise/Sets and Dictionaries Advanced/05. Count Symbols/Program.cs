907d334960fb3c47d639a21fc45abeaf4a86a219
zaprian1223 <zaprian1223@gmail.com>
2022-07-09 16:21:39 +0300

    Added a solution

---
+++ Excercise/Sets and Dictionaries Advanced/05. Count Symbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            SortedDictionary<char, int> dct = new SortedDictionary<char, int>();
+            foreach (var item in text)
+            {
+               
+                    if (dct.ContainsKey(item))
+                    {
+                        dct[item]++;
+                    }
+                    else
+                    {
+                        dct.Add(item, 1);
+                    }
+                
+            }
+            foreach (var item in dct)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
